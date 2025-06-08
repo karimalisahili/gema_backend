238e5f79a3a6c65f3122a9de9aac52a42d14b8b8
AndresChacon00 <andresroberto.c@gmail.com>
2025-06-07 22:45:42 -0400

    prettier config

--- src/tables/tecnico.ts
+++ src/tables/tecnico.ts
@@ -1,7 +1,7 @@
-import { serial, pgTable, varchar } from "drizzle-orm/pg-core";
+import { serial, pgTable, varchar } from 'drizzle-orm/pg-core';
 
-export const tecnico = pgTable("Tecnico", {
-    id: serial("id").primaryKey(),
-    nombre: varchar("nombre", { length: 50 }).notNull(),
-    correo: varchar("correo", { length: 40 }).notNull(),
+export const tecnico = pgTable('Tecnico', {
+  id: serial('id').primaryKey(),
+  nombre: varchar('nombre', { length: 50 }).notNull(),
+  correo: varchar('correo', { length: 40 }).notNull(),
 });
