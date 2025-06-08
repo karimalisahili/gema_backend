01fc056dc8f6f4664f3c8d2f2f2b83fae605947a
AndresChacon00 <andresroberto.c@gmail.com>
2025-06-07 22:42:56 -0400

    Tabla relacion TrabajaEnGrupo y not nulls

--- src/tables/grupoDeTrabajo.ts
+++ src/tables/grupoDeTrabajo.ts
@@ -4,8 +4,8 @@ import { pgTable, serial, varchar, integer } from "drizzle-orm/pg-core";
 import { tecnico } from "./tecnico";
 export const grupoDeTrabajo = pgTable("GrupoDeTrabajo",{
     id: serial("id").primaryKey(),
-    codigo: varchar("codigo", {length: 10}),
-    nombre: varchar("nombre", {length: 40}),
+    codigo: varchar("codigo", {length: 10}).notNull(),
+    nombre: varchar("nombre", {length: 40}).notNull(),
     supervisorId: integer("coodinadorId")
         .references(() => tecnico.id, { onDelete: "set null"})
 })
--- src/tables/tecnico.ts
+++ src/tables/tecnico.ts
@@ -2,6 +2,6 @@ import { serial, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const tecnico = pgTable("Tecnico", {
     id: serial("id").primaryKey(),
-    nombre: varchar("nombre", { length: 50 }),
-    correo: varchar("correo", { length: 40 }),
+    nombre: varchar("nombre", { length: 50 }).notNull(),
+    correo: varchar("correo", { length: 40 }).notNull(),
 });
