6b8befb4257c90ad9a2f10f5777c93ab38d2229f
AndresChacon00 <andresroberto.c@gmail.com>
2025-06-14 16:25:53 -0400

    nombre atributo

--- src/tables/grupoDeTrabajo.ts
+++ src/tables/grupoDeTrabajo.ts
@@ -4,7 +4,7 @@ export const grupoDeTrabajo = pgTable('GrupoDeTrabajo', {
   id: serial('id').primaryKey(),
   codigo: varchar('codigo', { length: 10 }).notNull(),
   nombre: varchar('nombre', { length: 40 }).notNull(),
-  supervisorId: integer('coodinadorId').references(() => usuarios.Id, {
+  supervisorId: integer('supervisor').references(() => usuarios.Id, {
     onDelete: 'set null',
   }),
 });
