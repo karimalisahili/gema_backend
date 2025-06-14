5eb969446ce15340efaee658897b7e371c870153
Roberth <roberthgraffegraffe@gmail.com>
2025-06-14 00:10:27 -0400

    crear ubicacione

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import tecnicoRoutes from '../controllers/tecnico/tecnico.routes';
 import authRoutes from '../controllers/auth/auth.routes';
+import ubicacionesTecnicasRoutes from '../controllers/ubicacionesTecnicas/ubicacionesTecnicas.routes';
 import { authenticate } from '../middleware/auth.middleware'; // Importa el middleware
 
 const router = Router();
@@ -8,5 +9,5 @@ const router = Router();
 // Protege la ruta de tecnicos
 router.use('/tecnicos', authenticate, tecnicoRoutes);
 router.use('/login', authRoutes);
-
+router.use('/ubicaciones-tecnicas', authenticate, ubicacionesTecnicasRoutes);
 export default router;
