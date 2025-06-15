fc5a40fecf48e028a4b3f57c0c3510f3ea0f9193
AndresChacon00 <andresroberto.c@gmail.com>
2025-06-15 01:06:55 -0400

    delete trabajaEnGrupo

--- src/controllers/trabajaEnGrupo/trabajaEnGrupo.routes.ts
+++ src/controllers/trabajaEnGrupo/trabajaEnGrupo.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { createTrabajaEnGrupoHandler } from './trabajaEnGrupo.controller';
+import {
+  createTrabajaEnGrupoHandler,
+  deleteTrabajaEnGrupoHandler,
+} from './trabajaEnGrupo.controller';
 
 const router = Router();
 
 router.post('/', createTrabajaEnGrupoHandler);
-
+router.delete('/:tecnicoId/:grupoDeTrabajoId', deleteTrabajaEnGrupoHandler);
 export default router;
--- src/controllers/trabajaEnGrupo/trabajaEnGrupo.service.ts
+++ src/controllers/trabajaEnGrupo/trabajaEnGrupo.service.ts
@@ -1,6 +1,8 @@
 import { db } from '../../db';
 import { trabajaEnGrupo } from '../../tables/trabajaEnGrupo';
 import { createTrabajaEnGrupoParams } from '../../types/trabajaEnGrupo';
+import { grupoDeTrabajo } from '../../tables/grupoDeTrabajo';
+import { and, eq } from 'drizzle-orm';
 
 export const createTrabajaEnGrupo = async (
   params: createTrabajaEnGrupoParams
@@ -19,11 +21,45 @@ export const createTrabajaEnGrupo = async (
     }
 
     return {
-      message: 'Trabajados añadido al grupo de trabajo',
+      message: 'Trabajador añadido al grupo de trabajo',
       trabajaEnGrupo: inserted[0],
     };
   } catch (error) {
     console.error('Error adding worker to group');
     throw new Error('Error al añadir trabajador al grupo de trabajo');
+  }
+};
+
+export const deleteTrabajaEnGrupo = async (
+  tecnicoId: number,
+  grupoDeTrabajoId: number
+) => {
+  try {
+    const deleted = await db
+      .delete(trabajaEnGrupo)
+      .where(
+        and(
+          eq(trabajaEnGrupo.tecnicoId, tecnicoId),
+          eq(trabajaEnGrupo.grupoDeTrabajoId, grupoDeTrabajoId)
+        )
+      )
+      .returning();
+
+    if (!deleted.length) {
+      throw new Error('Error al eliminar la relacion trabajaEnGrupo');
+    }
+
+    return {
+      message: 'Trabajador eliminado del grupo de trabajo',
+      trabajaEnGrupo: deleted[0],
+    };
+  } catch (error) {
+    console.error(
+      `Error eliminando asignación para tecnicoId ${tecnicoId} y grupoDeTrabajoId ${grupoDeTrabajoId}:`,
+      error
+    );
+    throw new Error(
+      'Error al eliminar la asignación del trabajador al grupo de trabajo'
+    );
   }
 };
