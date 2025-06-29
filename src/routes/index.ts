21dc69bdf6070bf7b423a94e68666a621414bece
Roberth <roberthgraffegraffe@gmail.com>
2025-06-29 16:50:00 -0400

    authenticate

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -12,5 +12,5 @@ router.use('/tecnicos', authenticate, tecnicoRoutes);
 router.use('/grupos', authenticate, grupoDeTrabajoRoutes);
 router.use('/trabajaEnGrupo', authenticate, trabajaEnGrupoRoutes);
 router.use('/login', authRoutes);
-router.use('/ubicaciones-tecnicas', ubicacionesTecnicasRoutes);
+router.use('/ubicaciones-tecnicas', authenticate, ubicacionesTecnicasRoutes);
 export default router;
