905e26cb1f1ad2e6fd089f6bcb018f3b38da5f9e
AndresChacon00 <andresroberto.c@gmail.com>
2025-07-03 11:55:21 -0400

    fix: codigo unico y tamaño del nombre

--- src/tables/grupoDeTrabajo.ts
+++ src/tables/grupoDeTrabajo.ts
@@ -2,8 +2,8 @@ import { pgTable, serial, varchar, integer } from 'drizzle-orm/pg-core';
 import { usuarios } from './usuarios';
 export const grupoDeTrabajo = pgTable('GrupoDeTrabajo', {
   id: serial('id').primaryKey(),
-  codigo: varchar('codigo', { length: 10 }).notNull(),
-  nombre: varchar('nombre', { length: 40 }).notNull(),
+  codigo: varchar('codigo', { length: 10 }).notNull().unique(),
+  nombre: varchar('nombre', { length: 100 }).notNull(),
   supervisorId: integer('supervisor').references(() => usuarios.Id, {
     onDelete: 'set null',
   }),
